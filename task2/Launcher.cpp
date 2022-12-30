07c086c01ab1e0c8a8b36361d8bb0f7958ac44de
Julianne Vella <54096586+julnnev@users.noreply.github.com>
2022-12-30 14:24:41 +0100

    Updated to launch game sequence

--- task2/Launcher.cpp
+++ task2/Launcher.cpp
@@ -4,8 +4,34 @@
 using namespace std;
 int main() {
     Game g;
-    g.playGame();
-   // b.initBoard();
-   // b.printBoard();
+    Board b;
+    int *p;
+    int x,y;
+
+   g.displayWelcomeMessage();
+   b.initBoard();
+   b.printFullBoard(); //for testing purposes
+
+   cout << "\n";
+
+
+   do{
+       p=g.enterCoords();
+       x=p[0];
+       y=p[1];
+
+       if(!b.isMine(x,y)){
+           b.displayBoard(x,y);
+       }
+
+       if(b.isMine(x,y)){
+           cout << "GAME OVER - You have encountered a mine!" <<endl;
+           b.printFullBoard();
+       }
+
+       // winning game output!
+
+   }while(!b.isMine(x,y));
+
     return 0;
 }
