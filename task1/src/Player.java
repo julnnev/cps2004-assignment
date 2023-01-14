69ffbba65a4ad26b41657dd5e80904af27ea7f30
Julianne Vella <54096586+julnnev@users.noreply.github.com>
2023-01-14 11:57:25 +0100

    Addition of AI boolean class attribute

--- task1/src/Player.java
+++ task1/src/Player.java
@@ -2,11 +2,12 @@ public class Player {
 
     public String name;
     public int playerNo; // digit displayed on map of villages
+    boolean AI;
 
-    public Player(String name) {
+    public Player(String name, boolean AI) {
         this.name = name;
         this.playerNo = Game.playerCount + 1;
-
+        this.AI=AI;
         //incrementing total amount of players
         Game.playerCount++;
     }
